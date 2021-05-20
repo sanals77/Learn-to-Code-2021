ca18e1ab66c325848cfd12d2a980c008f7b4187b
sanals77 <76899262+sanals77@users.noreply.github.com>
2021-05-20 20:36:25 +0530

    Create Triangle of numbers!.c

---
+++ Triangle of numbers!.c
@@ -0,0 +1,32 @@
+//Write a C program to print the following number pattern
+//55555
+// 4444
+//  333
+//   22
+//    1
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main() {
+   int a;
+    scanf("%d",&a);
+   for(int i=a;i>=1;i--)
+   {
+       for(int k=a-1;k>=i;k--)
+           printf(" ");
+       for(int j=1;j<=i;j++)
+       {
+           printf("%d",i);
+          
+       }
+      
+       
+       printf("\n");
+   }
+       return 0;
+   }
