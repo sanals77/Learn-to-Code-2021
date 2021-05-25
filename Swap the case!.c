43fab5ebd544b9a64ba6a4fa03127e38d02bfab7
sanals77 <76899262+sanals77@users.noreply.github.com>
2021-05-25 22:38:14 +0530

    Create Swap the case!.c

---
+++ Swap the case!.c
@@ -0,0 +1,19 @@
+//Write a C program to change the case of alphabets.
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() { char a[100];
+            gets(a);
+            for(int i=0;a[i]!='\0';i++)
+            {if(a[i]>='a' && a[i]<="z")
+             a[i]=a[i]-32;
+            else if(a[i]>='A' && a[i]<="Z")
+             a[i]=a[i]+32;}
+            puts(a);
+
+   
+    return 0;
+}
