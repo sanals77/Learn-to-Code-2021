3765c2a44d6d4d4c7dc8ed87b467e56096165d6f
sanals77 <76899262+sanals77@users.noreply.github.com>
2021-05-31 23:54:42 +0530

    Create Swap 22.c

---
+++ Swap 22.c
@@ -0,0 +1,18 @@
+//Write a C program to swap the values of two variables.
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {int a,b,a1,b1;
+            scanf("%d ",&a);
+             scanf("%d\n",&b);
+            a1=a;
+            b1=b;
+            printf("a=%d b=%d\n",a,b);
+            printf("a=%d b=%d",b1,a1);
+
+     
+    return 0;
+}
