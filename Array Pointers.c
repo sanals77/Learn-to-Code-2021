7db59c2e647470c8c99dbf53a4cf85ef764613de
sanals77 <76899262+sanals77@users.noreply.github.com>
2021-06-08 19:25:39 +0530

    Create Array Pointers.c

---
+++ Array Pointers.c
@@ -0,0 +1,31 @@
+//Write a C Program to read an array, then display the array twice. Once using the index, and the other time using the array pointer.
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+     int a[100],n;
+    
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int *b=a;
+    printf("DISPLAYING USING INDEX: ");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\nDISPLAYING USING POINTERS: ");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",*b);
+            b++;
+    }
+   
+    return 0;
+}
